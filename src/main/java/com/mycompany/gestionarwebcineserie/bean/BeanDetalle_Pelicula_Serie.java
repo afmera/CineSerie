09dda3d805b5e1b3af9629247503267710004049
afmera <felipemera@unimayor.edu>
2019-10-09 14:06:10 -0500

    SelectCheck genero

--- src/main/java/com/mycompany/gestionarwebcineserie/bean/BeanDetalle_Pelicula_Serie.java
+++ src/main/java/com/mycompany/gestionarwebcineserie/bean/BeanDetalle_Pelicula_Serie.java
@@ -25,18 +25,27 @@ import javax.faces.context.FacesContext;
 @ViewScoped
 public class BeanDetalle_Pelicula_Serie {
 
-    private Genero_Pelicula_Serie genero_pelicula_serie = new Genero_Pelicula_Serie();
+//    private Genero_Pelicula_Serie genero_pelicula_serie = new Genero_Pelicula_Serie();
     private Pelicula_Serie pelicula_serie = new Pelicula_Serie();
     private Genero genero = new Genero();
+    private String[] selectGenero=null;
     private List<Genero_Pelicula_Serie> listaGPS = new ArrayList<Genero_Pelicula_Serie>();
 
-    public Genero_Pelicula_Serie getGenero_pelicula_serie() {
-        return genero_pelicula_serie;
+    public String[] getSelectGenero() {
+        return selectGenero;
     }
 
-    public void setGenero_pelicula_serie(Genero_Pelicula_Serie genero_pelicula_serie) {
-        this.genero_pelicula_serie = genero_pelicula_serie;
+    public void setSelectGenero(String[] selectGenero) {
+        this.selectGenero = selectGenero;
     }
+
+//    public Genero_Pelicula_Serie getGenero_pelicula_serie() {
+//        return genero_pelicula_serie;
+//    }
+//
+//    public void setGenero_pelicula_serie(Genero_Pelicula_Serie genero_pelicula_serie) {
+//        this.genero_pelicula_serie = genero_pelicula_serie;
+//    }
 
     public Pelicula_Serie getPelicula_serie() {
         return pelicula_serie;
@@ -96,7 +105,6 @@ public class BeanDetalle_Pelicula_Serie {
      */
     public void quitarElemento(String valor) {
         try {
-//            boolean result = false;
             for (int cont = 0; cont < listaGPS.size(); cont++) {
                 System.out.println("\nlistaGPS.get(cont).getGenero().getNombre() "+listaGPS.get(cont).getGenero().getNombre()
                 +"\nvalor "+valor);
@@ -105,11 +113,6 @@ public class BeanDetalle_Pelicula_Serie {
                     FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "INFORMACION", "El registro a sido retirado de la tabla."));
                 }
             }
-//            if (result) {
-//                FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "INFORMACION", "El registro a sido retirado de la tabla."));
-//            } else {
-//                FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "INFORMACION", "El registro NO a sido retirado."));
-//            }
         } catch (Exception ex) {
             FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "ERROR", "Sea presentado un error en la lista.\n" + ex));
             System.out.println("Error es : " + ex);
